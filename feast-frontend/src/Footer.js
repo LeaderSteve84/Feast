ba892f3409269ce3036d61e5fd828d119faa663a
stephen_adah <mailstephenreal@gmail.com>
2024-03-05 16:10:59 +0100

    footer

--- feast-frontend/src/Footer.js
+++ feast-frontend/src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return ( 
         <nav className="footer">
             <a href="/home" className="footer-home">Home</a>
-            <a href="/foods" className="footer-foods">Foods</a> For the love of food . . .
+            <a href="/foods" className="footer-foods">Foods</a> <span className="mantra">For the love of food . . .</span>
         </nav>
      );
 }
