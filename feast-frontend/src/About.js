6dccde4b4a430f5dff6eedfd5bb02b3b02625378
stephen_adah <mailstephenreal@gmail.com>
2024-03-05 13:39:00 +0100

    about us

---
+++ feast-frontend/src/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div className="about">
+            <p className="about-head">
+                <span className="about-topic">About Feast?</span> A social web application platfrom for food lovers. For detail knowledge<span className="clickhere">Click Here.</span>
+            </p>
+        </div>
+     );
+}
+
+export default About;
