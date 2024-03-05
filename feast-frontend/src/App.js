bf316f3ef9c604589def51e5edfca84eaea88612
stephen_adah <mailstephenreal@gmail.com>
2024-03-05 14:51:14 +0100

    Wwedo

--- feast-frontend/src/App.js
+++ feast-frontend/src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 import Navbar from './Navbar';
 import About from './About';
 import Header from './Header';
+import Wwedo from './Wwedo';
 
 function App() {
   return (
     <div className="App">
-     <div className="header">
-      <Header />
-      <Navbar />
-     </div>
-     <div className="content">
-      <About />
-     </div>
+      <div className="headerContent">
+        <Header />
+      </div>
+      <div className="navContent">
+        <Navbar />
+      </div>
+      <div className="aboutContent">
+        <About />
+      </div>
+      <div className="moreContent">
+        <Wwedo />
+      </div>
     </div>
   );
 }
---
+++ feast-frontend/src/Wwedo.js
@@ -0,0 +1,11 @@
+const Wwedo = () => {
+    return (
+        <p className="wwedo">
+            <span className="wwd">What We Do?</span><br />
+            <span className="answer">- - We provides food lovers accross the globe, with information about various kinds of foods with their recipes.</span><br />
+            <span className="more">More</span> about what we do.
+        </p>
+    );
+}
+ 
+export default Wwedo;
