63b9589a6e3cf1420540cb739f3dbd6037368224
stephen_adah <mailstephenreal@gmail.com>
2024-03-05 15:28:43 +0100

    displayfood

--- feast-frontend/src/DisplayFood.js
+++ feast-frontend/src/DisplayFood.js
@@ -1,13 +1,13 @@
 const DisplayFood = () => {
 
-    foodName = "Maize Poddy";
-    recipes = ["Curry", "Thyme", "Knor", "Chili pepper", "Refined Vegetable Oil" ];
-    
+    let foodName = "Maize Poddy";
+    let recipes = ["Curry, ", "Thyme, ", "Knor, ", "Chili pepper, ", "Refined Vegetable Oil,"]
+    let foodImage = ""
     return ( 
         <div className="displayfoods">
             <p className="pointer">See <img src="" alt="see below" className="image-pointer"/> sample of foods and their recipes.</p>
             <div className="foodImages">
-                <img src="" alt="Sample foods and recipes" />
+                <img src= { foodImage } alt="Sample foods and recipes" />
             </div>
             <p>
                 <span className="name">Name: { foodName } </span><br />
