dc5e0a180851a797f8b6b52ae5e91f4b2472ada4
stephen_adah <mailstephenreal@gmail.com>
2024-03-08 18:28:14 +0100

    footer.js

--- feast-frontend/src/Footer.js
+++ feast-frontend/src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer"> 
             <nav>
-                <Link to="/home" className="footer-home">Home</Link>
+                <Link to="/" className="footer-home">Home</Link>
                 <Link to="/foods" className="footer-foods">Foods</Link> 
                 <Link to="/about" className="footer-about">About</Link> 
             </nav>
