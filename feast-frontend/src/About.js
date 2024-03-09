058994a97b49baf256a019bd86d6583de978d45e
stephen_adah <mailstephenreal@gmail.com>
2024-03-09 08:46:42 +0100

    about.js

--- feast-frontend/src/About.js
+++ feast-frontend/src/About.js
@@ -1,7 +1,56 @@
 const About = () => {
     return ( 
         <div className="about">
-            <h1>This is the about page</h1>
+            <h3 className="paragraph-heading">Introducing Feast: Your Culinary Companion</h3>
+            <p className="paragraph">
+                Are you a passionate food enthusiast looking for a platform that caters to your love for<br />
+                culinary adventures? Look no further than Feast - your ultimate destination for discovering<br />
+                delightful meals, exploring tantalizing recipes, and engaging with a vibrant community of<br />
+                fellow food lovers.
+            </p>
+            <h3 className="paragraph-heading">Discover Delectable Delights</h3>
+            <p className="paragraph">
+                Feast offers a treasure trove of diverse and mouthwatering meals curated to satisfy every<br />
+                palate. Whether you're craving a comforting bowl of homemade soup, a sizzling stir-fry, or an<br />
+                indulgent dessert to satisfy your sweet tooth, Feast has you covered. With a vast array of<br />
+                recipes from around the globe, you'll never run out of inspiration for your next culinary<br />
+                creation.
+            </p>
+            <h3 className="paragraph-heading">Unlock Inspiring Recipes</h3>
+            <p className="paragraph">
+                Feast isn't just about browsing delicious dishes - it's about empowering you to unleash your<br />
+                inner chef. Dive into our extensive collection of recipes, thoughtfully crafted to guide you<br />
+                through every step of the cooking process. From beginner-friendly basics to gourmet delights,<br />
+                our recipes cater to all skill levels and dietary preferences. With detailed instructions,<br />
+                ingredient lists, and helpful tips, you'll be whipping up restaurant-worthy meals in no time.
+            </p>
+
+            <h3 className="paragraph-heading">Engage and Connect</h3>
+            <p className="paragraph">
+                At Feast, we believe that food is more than just sustenance - it's a catalyst for connection<br />
+                and community. Join our vibrant community of food enthusiasts to share your culinary<br />
+                triumphs, exchange recipe recommendations, and connect with like-minded individuals who<br />
+                share your passion for good food. Whether you're seeking advice, sharing your latest kitchen<br />
+                experiments, or simply swapping stories over a virtual meal, Feast is the place to be.   
+            </p>
+
+            <h3 className="paragraph-heading">Leave Your Mark with Comments</h3>
+            <p className="paragraph">
+                Have a favorite recipe that you can't get enough of? Or perhaps you've discovered a clever<br />
+                twist on a classic dish that you're eager to share? With Feast's interactive commenting<br />
+                feature, you can engage with recipes and meals like never before. Leave feedback, share tips,<br />
+                or simply express your appreciation for a delicious creation - your voice matters, and your<br />
+                comments help enrich the Feast experience for everyone. 
+            </p>
+
+            <h3 className="paragraph-heading">Join the Feast</h3>
+            <p className="paragraph">
+                Ready to embark on a culinary journey like no other? Whether you're a seasoned home chef or<br />
+                a curious foodie eager to explore new flavors, Feast invites you to join our ever-growing<br />
+                community of food lovers. Discover, create, and connect with us as we celebrate the art of<br />
+                cooking and the joy of sharing delicious meals with the ones we love. Welcome to Feast - <br />
+                where every meal is a celebration of flavor, creativity, and community. Bon appétit!
+            </p>
         </div>
      );
 }
--- feast-frontend/src/DisplayFood.js
+++ feast-frontend/src/DisplayFood.js
@@ -2,12 +2,12 @@ const DisplayFood = () => {
 
     let foodName = "Maize Poddy";
     let recipes = ["Curry, ", "Thyme, ", "Knor, ", "Chili pepper, ", "Refined Vegetable Oil,"]
-    let foodImage = ""
+    // let foodImage = <img src= alt="" />
     return ( 
         <div className="displayfoods">
             <p className="pointer">See <img src="" alt="see below" className="image-pointer"/> sample of foods and their recipes.</p>
             <div className="foodImages">
-                <img src= { foodImage } alt="Sample foods and recipes" />
+                <link rel="apple-touch-icon" href="%PUBLIC_URL%/food_a.jpg" alt="Sample foods and recipes" />
             </div>
             <p className="name-recipes-comment">
                 <span className="name">Name: { foodName } </span><br />
