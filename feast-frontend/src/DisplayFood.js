3dc4eab90032b4bfdc0e92fafec29fa10c09718b
stephen_adah <mailstephenreal@gmail.com>
2024-03-09 09:41:46 +0100

    DisplayFood

--- feast-frontend/src/DisplayFood.js
+++ feast-frontend/src/DisplayFood.js
@@ -2,18 +2,21 @@ import food_a from './foodimages/food_a.jpg'
 
 const DisplayFood = () => {
 
-    let foodName = "Maize Poddy";
-    let recipes = ["Curry, ", "Thyme, ", "Knor, ", "Chili pepper, ", "Refined Vegetable Oil,"]
-    // let foodImage = <img src= alt="" />
+    const foodImageData = [
+        <img src={ food_a } alt="sample food" height={400} width={400} />, 
+        "Maize Poddy", 
+        ["Curry, ", "Thyme, ", "Knor, ", "Chili pepper, ", "Refined Vegetable Oil,"] 
+    ]
+        
     return ( 
         <div className="displayfoods">
             <p className="pointer">See <img src="" alt="see below" className="image-pointer"/> sample of foods and their recipes.</p>
             <div className="foodImages">
-                <img src={ food_a } alt="sample food" height={400} width={400} />
+                { foodImageData[0] }
             </div>
             <p className="name-recipes-comment">
-                <span className="name">Name: { foodName } </span><br />
-                <span className="recipes">Recipes: { recipes } </span><br />
+                <span className="name">Name: { foodImageData[1] } </span><br />
+                <span className="recipes">Recipes: { foodImageData[2] } </span><br />
                 <span className="homeComment">Comment:</span> <input type="text" className="input" />
             </p>
         </div>
